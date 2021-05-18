964d9f3e3f8c9c85841c8bbbec83dd438e24e8e8
Krzysztof Baranski <krisbaranski@gmail.com>
2021-05-18 14:28:54 +0200

    initial commit

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const btn = document.querySelector('.btn');
 // const boxes = document.querySelectorAll('.box');
 const box = document.querySelector('.box');
 const container = document.getElementById('container');
+const postcard = document.querySelector('.postcard');
+// API request
+const API_URL = 'https://api.teleport.org/api/urban_areas';
 
 function removeAllBoxes() {
   while (container.hasChildNodes()) {
@@ -27,7 +30,6 @@ function addClickEventToBoxes() {
   let boxes = document.getElementsByClassName('box');
   for (let box of boxes) {
     box.addEventListener('click', showPostcard());
-    console.log('hello');
   }
 }
 
@@ -35,12 +37,28 @@ function displayBoxes() {
   let number = Number(document.getElementById('amount').value);
   removeAllBoxes();
   addBoxes(number);
+  showImages();
   addClickEventToBoxes();
 }
 
+async function getImages(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  showImages(data.results);
+}
+
+function showImages() {
+  getImages(API_URL);
+}
+
 function showPostcard() {
-  const postcard = document.querySelector('.postcard');
   postcard.classList.remove('hidden');
+
+  // if (postcard.classList.contains('hidden')) {
+  // } else {
+  //   postcard.classList.add('hidden');
+  // }
 }
 
 btn.addEventListener('click', displayBoxes);
